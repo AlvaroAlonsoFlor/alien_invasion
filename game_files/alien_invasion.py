c06a5b93d56f27868395b4cda585d6be83d777d7
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-11-12 21:23:44 +0000

    Create basic settings

--- game_files/alien_invasion.py
+++ game_files/alien_invasion.py
@@ -1,2 +1,20 @@
 import sys
 import pygame
+
+def run_game():
+    pygame.init()
+    #screen settings
+    screen = pygame.display.set_mode((1920, 1080))
+    pygame.display.set_caption("Alien Invasion")
+    bg_color = (46, 55, 71)
+
+    #condition to run
+    while True:
+        # keyboard and mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        # flip == display most recent screen drawn
+        pygame.display.flip()
+
+run_game()
