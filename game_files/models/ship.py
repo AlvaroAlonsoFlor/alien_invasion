fd8be70fc1ca5238561b016ac2184635d6283470
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-11-13 11:43:44 +0000

    Solve path problem loading images

--- game_files/models/ship.py
+++ game_files/models/ship.py
@@ -1,10 +1,14 @@
 import pygame
+import pygame.image
+import os
 
 class Ship():
 
     def __init__(self, screen):
         self.screen = screen
-        self.image = pygame.image.load('../images/ship.bmp')
+        
+        img_path =  os.path.abspath("game_files/images")
+        self.image = pygame.image.load(img_path + '/player-ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
