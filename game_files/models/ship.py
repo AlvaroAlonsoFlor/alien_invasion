fa0dc1bebc576116c1f95ebca92c383b327629f6
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-11-13 11:51:43 +0000

    Transform ship to adapt the screen size

--- game_files/models/ship.py
+++ game_files/models/ship.py
@@ -7,8 +7,11 @@ class Ship():
     def __init__(self, screen):
         self.screen = screen
         
+        # import and scale image
         img_path =  os.path.abspath("game_files/images")
         self.image = pygame.image.load(img_path + '/player-ship.bmp')
+        self.image = pygame.transform.scale(self.image, (60, 60))
+
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
