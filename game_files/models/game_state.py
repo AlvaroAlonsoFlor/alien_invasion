48632f2588d580fd74a25f193557a2cee611ec46
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-12-04 14:03:31 +0000

    Add update_high_score method to GameState

--- game_files/models/game_state.py
+++ game_files/models/game_state.py
@@ -13,9 +13,22 @@ class GameState():
         path = os.path.abspath('score.txt')
         file = open(path, 'r')
         self.high_score = file.read()
+        file.close()
         
     
     def reset_stats(self):
         self.score = 0
         self.ships_left = self.settings.ship_limit
+    
+    def update_high_score(self, new_score):
+
+        if self.high_score == '':
+            self.high_score = 0
+
+        if int(self.high_score) < int(new_score):
+            path = os.path.abspath('score.txt')
+            file = open(path, 'w')
+            file.write(str(new_score))
+            file.close()
+
         
